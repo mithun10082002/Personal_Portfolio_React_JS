994ee158d96f5fa030faf7b2e6e5d6cac8719787
Mithun Raj <smithunraj007@gmail.com>
2023-07-04 21:20:20 +0530

    portfolio

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,53 @@
+import pro1 from "../assets/Project1.jpg";
+import pro2 from "../assets/Project2.jpg";
+import pro3 from "../assets/Project3.jpg";
+import pro4 from "../assets/Project4.jpg";
+import pro5 from "../assets/Project5.jpg";
+import pro6 from "../assets/Project6.jpg";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "Personal Portfolio",
+    text: "Simple portfolio Design in figma.",
+    view: "",
+    source: "https://github.com/mithun10082002/PersonalPortfolio-Django",
+  },
+  {
+    imgsrc: pro2,
+    title: "Password Generator",
+    text: "Password generator website using Django.",
+    view: "",
+    source: "https://github.com/mithun10082002/PasswordGenerator-Django",
+  },
+  {
+    imgsrc: pro3,
+    title: "To-Do",
+    text: "Simple Todo list website using React JS.",
+    view: "",
+    source: "",
+  },
+  {
+    imgsrc: pro4,
+    title: "Heart Disease Prediction",
+    text: "A website to predict whether the person is having heart disease or not using Machine Learning.",
+    view: "",
+    source: "https://github.com/mithun10082002/Heart-Disease-Prediction/tree/main/HeartPrediction",
+  },
+  {
+    imgsrc: pro5,
+    title: "Diabetes Prediction",
+    text: "A website to predict whether the person is having diabetes or not using Machine Learning.",
+    view: "",
+    source: "",
+  },
+  {
+    imgsrc: pro6,
+    title: "Personal Portfolio",
+    text: "Simple portfolio using React JS.",
+    view: "",
+    source: "",
+  },
+];
+
+export default ProjectCardData;
