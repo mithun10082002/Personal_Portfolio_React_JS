84bd44b392232cc44e2594f08c87b569f4416e42
Mithun Raj <smithunraj007@gmail.com>
2023-11-20 12:28:33 +0530

    Personal Portfolio

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -4,6 +4,12 @@ import pro3 from "../assets/Project3.jpg";
 import pro4 from "../assets/Project4.jpg";
 import pro5 from "../assets/Project5.jpg";
 import pro6 from "../assets/Project6.jpg";
+import pro7 from "../assets/Project7.png";
+import pro8 from "../assets/Project8.png";
+import pro9 from "../assets/Project9.png";
+import pro10 from "../assets/Project10.png";
+import pro11 from "../assets/Project11.png";
+import pro12 from "../assets/Project12.png";
 
 const ProjectCardData = [
   {
@@ -32,7 +38,8 @@ const ProjectCardData = [
     title: "Heart Disease Prediction",
     text: "A website to predict whether the person is having heart disease or not using Machine Learning.",
     view: "",
-    source: "https://github.com/mithun10082002/Heart-Disease-Prediction/tree/main/HeartPrediction",
+    source:
+      "https://github.com/mithun10082002/Heart-Disease-Prediction/tree/main/HeartPrediction",
   },
   {
     imgsrc: pro5,
@@ -47,6 +54,48 @@ const ProjectCardData = [
     text: "Simple portfolio using React JS.",
     view: "",
     source: "",
+  },
+  {
+    imgsrc: pro7,
+    title: "Todo App",
+    text: "To-Do List using HTML, CSS and JavaScript...",
+    view: "",
+    source: "https://github.com/mithun10082002/To-Do-List",
+  },
+  {
+    imgsrc: pro8,
+    title: "Calculator",
+    text: "Calculator using HTML, CSS and JavaScript...",
+    view: "",
+    source: "https://github.com/mithun10082002/Calculator",
+  },
+  {
+    imgsrc: pro9,
+    title: "Quiz App",
+    text: "Quiz App using HTML, CSS and JavaScript...",
+    view: "",
+    source: "https://github.com/mithun10082002/Quiz-App",
+  },
+  {
+    imgsrc: pro10,
+    title: "Dark/Light-Mode",
+    text: "Dark/Light-Mode using HTML, CSS and JavaScript...",
+    view: "",
+    source: "https://github.com/mithun10082002/Dark-Light-Mode",
+  },
+  {
+    imgsrc: pro11,
+    title: "Netflix Clone App",
+    text: "Netflix Clone App using HTML and CSS",
+    view: "",
+    source: "https://github.com/mithun10082002/Netflix-Clone-App",
+  },
+  {
+    imgsrc: pro12,
+    title: "Foody Site",
+    text: "Foody Site using HTML and CSS",
+    view: "",
+    source: "https://github.com/mithun10082002/Foody-Site",
   },
 ];
 
